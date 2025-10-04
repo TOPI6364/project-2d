9db7ae20086ac7fecc085961e93c50cf0f2fdaa1
ilusha <sheludkoilyansk@gmail.com>
2025-10-04 18:33:10 +0300

    Eshkere

--- Assets/scripts/Sup.cs
+++ Assets/scripts/Sup.cs
@@ -7,9 +7,13 @@ public class Sup : MonoBehaviour
 {
     [SerializeField] float stopDistance = 2f;
     NavMeshAgent agent;
+    Animator anim;
+    float StartScale;
 
     void Start()
     {
+        StartScale = transform.localScale.x;
+        anim = GetComponentInChildren<Animator>();
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
@@ -25,13 +29,24 @@ public class Sup : MonoBehaviour
     void Update()
     {
         float distance = Vector3.Distance(transform.position, GameManager.Instance.player.transform.position);
+        if (GameManager.Instance.player.transform.position.x - transform.position.x < 0)
+        {
+            transform.localScale = new Vector3(StartScale, StartScale, StartScale);
+        }
+        else if (GameManager.Instance.player.transform.position.x - transform.position.x > 0)
+        {
+            transform.localScale = new Vector3(-StartScale, StartScale, StartScale);
+        }
+
         if (distance > stopDistance)
         {
             agent.SetDestination(GameManager.Instance.player.transform.position);
+            anim.SetBool("1_Move", true);
         }
         else
         {
             agent.ResetPath();
+            anim.SetBool("1_Move", false);
         }
     }
 
